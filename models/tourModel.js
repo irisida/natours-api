2f235c4b123b243fe5f7cbebcdf9648d59c9a7fe
irisida <20445648+irisida@users.noreply.github.com>
2021-03-23 22:55:51 +0000

    bulk up the tour model with extra fields

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,55 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     unique: true,
-    required: [true, 'A tour must have a name property'],
+    trim: true,
+    required: [true, 'Tour records must have a name property'],
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour records must have a duration.'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour records must have a maximum group size set.'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour records must have a difficulty setting'],
+  },
+  ratingAverage: {
     type: Number,
     default: 5,
   },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price associated to it'],
+    required: [true, 'Tour records must have a price for the tour'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour records must have a description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour records must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
