387b3e1539510bc89009d064a65d0dec101c14f4
irisida <20445648+irisida@users.noreply.github.com>
2021-03-24 22:14:35 +0000

    added import, delete and reset script for data loading and cleansing

---
+++ dev-data/data/importData.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: `${__dirname}/../../config.env` });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data uploaded');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const resetData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfuly DELETED!');
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//ADD AN ADDITIONAL ELSE IF
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+} else if (process.argv[2] === '--reset') {
+  resetData();
+}
